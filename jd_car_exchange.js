9efd62ce996d35240dfc9e0f2b008aaf3a14613e
lan-tianxiang <lan-tianxiang@github.com>
2022-05-29 00:00:00 +0800

    .

--- jd_car_exchange.js
+++ jd_car_exchange.js
@@ -82,7 +82,7 @@ function showMsg() {
 
 function exchange() {
   return new Promise(resolve => {
-    $.post(taskUrl('v1/user/exchange/bean'), (err, resp, data) => {
+    $.post(taskUrl('v1/user/exchange/bean/check'), (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
